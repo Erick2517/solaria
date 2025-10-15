054d5d5c3c10e3aa839ff78e87cd747a740ae583
Jose Garcia <josegarciarrr9@gmail.com>
2025-10-15 17:28:45 -0600

    archivo corregido, la ruta del controlador

--- prueba.php
+++ prueba.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'controllers_LANDAVERDE/MarcaController.php';
+require_once 'controllers/MarcaController.php'; //las rutas son importantes tambien
 $controller=new MarcaController();
 
 $controller->listarMarcas();
