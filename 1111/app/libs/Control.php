0480cc2c0cbc660ec01459c10a484d0a85f7ed09
Jose Garcia <josegarciarrr9@gmail.com>
2025-10-15 17:29:43 -0600

    clase control, se creó el método de separarURL

--- 1111/app/libs/Control.php
+++ 1111/app/libs/Control.php
@@ -1,8 +1,32 @@
 <?php
+/**
+ * contro que maneja la url y lanza los procesos
+ * el primer elemento es el objeto o controlador
+ * el segundo es el metodo o accion
+ * y el tercer y posteriores son los parametros que se pasan
+ */
  class Control
 {
     function __construct()
     {
-        $db= new MySQLdb();
+        $url=$this->separarURL();
+        var_dump($url);
+
     }
+    function separarURL(){
+        $url= "";
+        if(isset($_GET["url"])){
+            //eliminamos el caracter final
+            $url=rtrim($_GET["url"], "/");
+            $url = rtrim($_GET["url"], "\\");
+            ///limpíamos caracteres que no son propios para la url
+            $url= filter_var($url, FILTER_SANITIZE_URL);
+            //separamos
+            $url=explode("/",$url);
+            return $url;
+
+        }
+    }
+
+
 }
